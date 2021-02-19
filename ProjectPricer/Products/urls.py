e9072cb7c994789aeca5f2195ce639c68a0e3ac1
NBaiel81 <nurmatbekov0771503907@gmail.com>
2021-02-19 18:23:39 +0600

    what can I do sometimes

--- ProjectPricer/Products/urls.py
+++ ProjectPricer/Products/urls.py
@@ -3,6 +3,6 @@ from django.contrib.auth import views as auth_views
 from .views import *
 urlpatterns=[
     path('',homepage,name='home'),
-    path('product_list/<int:firm_id>/',product_list,name='product_list')
-
+    path('product_list/<int:firm_id>/',product_list,name='product_list'),
+    path('models/<int:product_id>/',product_models,name='product_models')
 ]
--- ProjectPricer/Products/views.py
+++ ProjectPricer/Products/views.py
@@ -7,6 +7,10 @@ def homepage(request):
 
 def product_list(request,firm_id):
     firm = Firm.objects.get(id=firm_id)
-    products= firm.product_set.all
+    products= firm.product_set.all()
     return render(request, 'products/product_list.html', {'products': products})
 
+def product_models(request,product_id):
+    product = Product.objects.get(id=product_id)
+    p_models = product.product_model.all()
+    return render(request, 'products/product_detail.html', {'product': product,'models':p_models})
